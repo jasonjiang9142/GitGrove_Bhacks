8a990be3b12acb3c9d82fecb9d0b164eb3d18874
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2023-11-18 15:28:20 -0500

    signup form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import MyForm from './components/form';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex justify-center items-center h-screen bg-red-300">
+      <div className='bg-blue-300 p-6 rounded-md shadow-md text-center rounded-xl'>
+        <h1 className="text-3xl font-bold mb-4">Submit Your GitHub Page</h1>
+        <MyForm />
+      </div>
+    </div>
+  );
 }
